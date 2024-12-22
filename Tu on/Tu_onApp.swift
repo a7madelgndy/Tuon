fbfa083cb0c76f55f2b07a12660975c0a13f8de9
Ahmadelgndy <74977392+a7madelgndy@users.noreply.github.com>
2024-12-22 13:40:53 +0200

    Initial Commit

---
+++ Tu on/Tu_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tu_onApp.swift
+//  Tu on
+//
+//  Created by Ahmed El Gndy on 22/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Tu_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
